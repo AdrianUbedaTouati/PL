bc1d110ac428f7c0a20905161ba29a214e732204
AdrianUbedaTouati <aut3@alu.ua.es>
2023-02-22 18:49:54 +0100

    siguienteCaracter en proceso

--- src/AnalizadorLexico.java
+++ src/AnalizadorLexico.java
@@ -1,7 +1,20 @@
+import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class AnalizadorLexico {
-    AnalizadorLexico(RandomAccessFile entrada){
-
+    RandomAccessFile texto;
+    AnalizadorLexico(RandomAccessFile entrada) {
+        texto = entrada;
+    }
+    public String siguienteToken(){
+        String token = new String();
+        try{
+            int caracterAscii = texto.read();
+            String caracterString = Character.toString((char) caracterAscii);
+            return caracterString;
+        } catch (IOException e) {
+            System.out.println("Error al leer el fichero" + e.getMessage());
+        }
+        return token;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,16 @@ class plp1 {
                 RandomAccessFile entrada = new RandomAccessFile(args[0],"r");
                 AnalizadorLexico al = new AnalizadorLexico(entrada);
                 AnalizadorSintacticoDR asdr = new AnalizadorSintacticoDR(al);
+                //añadido
+                System.out.println(al.siguienteToken());
 
                 asdr.S(); // simbolo inicial de la gramatica
                 asdr.comprobarFinFichero();
             }
             catch (FileNotFoundException e) {
                 System.out.println("Error, fichero no encontrado: " + args[0]);
+            } catch (IOException e) {
+                System.exit(-1);
             }
         }
         else System.out.println("Error, uso: java plp1 <nomfichero>");
